f1fcd0ae337a38c029fc93b19a68ec082d1fc348
Arnoud de Boer <a.deboer@geonovum.nl>
2017-11-09 09:50:28 +0100

    Respec config

---
+++ documentatie/1.0.2/afbeeldingen/geobag-colofon.js
@@ -0,0 +1,29 @@
+var respecConfig = {
+  specStatus: "GN-VV",
+  specType: "ST",
+  //canonicalURI: "WV",
+  publishDate: "2017-11-10",
+  editors: [
+    {
+      name: "Arnoud de Boer",
+      company: "Geonovum",
+      companyURL: "http://www.geonovum.nl/",
+      mailto: "a.deboer@geonovum.nl",
+      note: ""    
+    }  
+  ],
+  //shortName: "shortname",
+  pubDomain: "http://www.geonovum.nl/wegwijzer/standaarden/gegevenscatalogus-bgt-111",
+  github: "https://github.com/Geonovm/geobag",
+  //previousPublishDate: "2017-01-01",
+  //previousStatus: "GN-CV",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+};
