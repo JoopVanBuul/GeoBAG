96f640f744d451697fe49ad8ce9dc43a5a85f864
Arnoud de Boer <a.deboer@geonovum.nl>
2017-11-10 12:03:37 +0100

    Update colofon.js

--- documentatie/1.0.1/media/colofon.js
+++ documentatie/1.0.1/media/colofon.js
@@ -12,19 +12,7 @@ var respecConfig = {
       note: ""    
     }  
   ],
-  shortName: "standaard-berichtenverkeer",
+  shortName: "berichtenverkeer",
   pubDomain: "geobag", 
-  github: "https://github.com/Geonovum/geobag",
-  //previousURI: "https://www.geonovum.nl/wegwijzer/standaarden/berichtenverkeer-stuf-geo-bag-geo-bag-berichtenverkeer-v10",
- // previousPublishDate: "2015-12-10",
- // previousStatus: "GN-DEF",
-  //localBiblio: {
-  //  "ISO19136": {
-  //    "href": "https://www.iso.org/standard/32554.html",
-  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
-  //    "authors": [""],
-  //    "date": "September 2007",
-  //    "publisher": "International Organization for Standardization",
-  //  },
-  //},
+  github: "https://github.com/Geonovum/geobag", 
 };
