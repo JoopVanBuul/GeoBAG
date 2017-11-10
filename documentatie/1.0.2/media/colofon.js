bbc403a6a2f8b9364eb0491a3072b8d9fc3eba09
Arnoud de Boer <a.deboer@geonovum.nl>
2017-11-10 11:57:06 +0100

    Update colofon.js

--- documentatie/1.0.2/media/colofon.js
+++ documentatie/1.0.2/media/colofon.js
@@ -12,11 +12,11 @@ var respecConfig = {
       note: ""    
     }  
   ],
-  shortName: "Geo-BAG berichtenverkeer v1.0.2",
+  shortName: "standaard",
   pubDomain: "geobag", 
-  previousURI: "https://docs.geostandaarden.nl/geobag/Geo-BAG berichtenverkeer v1.0.2/",
+  //previousURI: "https://docs.geostandaarden.nl/geobag/Geo-BAG berichtenverkeer v1.0.2/",
   github: "https://github.com/Geonovum/geobag",
-  previousPublishDate: "2017-08-01",
+  //previousPublishDate: "2017-08-01",
   //previousStatus: "GN-DEF",
   //localBiblio: {
   //  "ISO19136": {
