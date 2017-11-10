d693e32d9ec0e4227f79340e25b045d02a5d912d
Arnoud de Boer <a.deboer@geonovum.nl>
2017-11-10 08:51:00 +0100

    Update geobag-colofon.js

--- documentatie/1.0.1/media/geobag-colofon.js
+++ documentatie/1.0.1/media/geobag-colofon.js
@@ -1,3 +1,5 @@
+
+
 var respecConfig = {
   specStatus: "GN-DEF",
   specType: "ST",
@@ -13,11 +15,10 @@ var respecConfig = {
     }  
   ],
   shortName: "Geo-BAG berichtenverkeer v1.0.1",
-  pubDomain: "geobag",
-  edDraftURI: "",
+  pubDomain: "geobag", 
   previousURI: "https://www.geonovum.nl/wegwijzer/standaarden/berichtenverkeer-stuf-geo-bag-geo-bag-berichtenverkeer-v10",
   github: "https://github.com/Geonovum/geobag",
-  previousPublishDate: "2017-12-10",
+  previousPublishDate: "2015-12-10",
   previousStatus: "GN-DEF",
   //localBiblio: {
   //  "ISO19136": {
